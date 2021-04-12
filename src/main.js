78a27f0e4f2554e840c37223304552b3d865528b
mankaz <m.kazemi6222@gmail.com>
2021-04-12 14:59:08 +0430

    Add Online Chat

--- src/main.js
+++ src/main.js
@@ -6,19 +6,26 @@ import VueRouter from 'vue-router';
 import router from "./router";
 import VueApexCharts from 'vue-apexcharts'
 import VuePersianDatetimePicker from 'vue-persian-datetime-picker';
+
 Vue.component('date-picker', VuePersianDatetimePicker);
 import VueStore from '@websanova/vue-store';
 import vueResource from 'vue-resource'
 
-
-Vue.use(Buefy);
-Vue.use(VueRouter);
-Vue.use(vueResource);
-Vue.use(VueStore);
-Vue.use(VuePersianDatetimePicker);
-Vue.use(VueApexCharts);
+Vue.use(VueRouter)
+Vue.use(
+    Buefy,
+    vueResource,
+    VueStore,
+    VuePersianDatetimePicker,
+    VueApexCharts
+);
+// Vue.use(VueRouter);
+// Vue.use(vueResource);
+// Vue.use(VueStore);
+// Vue.use(VuePersianDatetimePicker);
+// Vue.use(VueApexCharts);
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
+    el: '#app',
+    render: h => h(App),
+    router
 })
